63f756400f78902205e6bff65cc30d3ca3e8cb1c
rafi-aman3 <rafiaman03@gmail.com>
2025-03-26 17:24:45 +0600

    fix: category dropdown issue

--- src/components/CategoryDropdown.jsx
+++ src/components/CategoryDropdown.jsx
@@ -1,22 +1,24 @@
+import { Link } from "react-router";
 import useCategories from "../hooks/useCategories";
 
 const CategoryDropdown = () => {
 	const { categories } = useCategories();
 
 	return (
-		<li>
-			<details>
-				<summary>Categories</summary>
-				<ul className='w-[300px] right-0 z-[100] bg-base-100 rounded-t-none shadow-md p-2'>
-					{categories?.map((category) => {
-						return (
-							<li key={category.id}>
-								<a href={`/categories/${category.id}`}>{category.name}</a>
-							</li>
-						);
-					})}
-				</ul>
-			</details>
+		<li className='dropdown dropdown-bottom dropdown-end'>
+			<div tabIndex={0} role='button' className='btn m-1'>
+				Categories
+			</div>
+			<ul
+				tabIndex={0}
+				className='dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52'
+			>
+				{categories?.map((category) => (
+					<li onClick={() => document.activeElement.blur()} key={category.id}>
+						<Link to={`/categories/${category.id}`}>{category.name}</Link>
+					</li>
+				))}
+			</ul>
 		</li>
 	);
 };
